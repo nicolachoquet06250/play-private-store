2a2cac32439d949666549a883569c12d8f3e932f
Nicolas Choquet <nicolachoquet06250@gmail.com>
2021-11-26 16:28:31 +0100

    fix page ShowApp

--- src/views/ShowApp.vue
+++ src/views/ShowApp.vue
@@ -7,7 +7,7 @@
                         <div class="app-icon"></div>
                     </div>
 
-                    <div>
+                    <div style="max-width: 250px;">
                         <ion-list class="app-details">
                             <ion-item>
                                 <h1 class="app-title"> {{ app.name }} </h1>
