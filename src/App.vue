6e3201750444a5fcac37aaecda3ef86376131636
Nicolas Choquet <nicolachoquet06250@gmail.com>
2021-12-07 19:39:02 +0100

    mise à jour des actions liées au websocket

--- src/App.vue
+++ src/App.vue
@@ -30,63 +30,73 @@
 
 <script setup>
 import { Sidebar, Toast, Loader } from '@/components';
-import { ref, computed } from 'vue';
-import { useResponsive, useSearchbar, useWebsocket } from '@/hooks';
+import { ref, computed, watch } from 'vue';
+import { useResponsive, useSearchbar, useWebsocket, useGuest, useToast, useApp } from '@/hooks';
 import env from '../env.json';
 
 const { resize, xs, sm } = useResponsive();
 const { showed } = useSearchbar();
-const { connect, addWsEventListener } = useWebsocket(env.SOCKET_URL);
+const { connect, disconnect, addWsEventListener } = useWebsocket(env.SOCKET_URL);
+const { openToast } = useToast();
+const { isSignedIn } = useGuest();
 
 const header = ref();
 const offsetHeight = computed(() => (header.value?.offsetHeight ?? 0) + 'px');
 const marginTop = ref('50px');
 const searchBarWidth = ref('calc(100% - 60px)');
 
-connect();
+watch(isSignedIn, () => {
+  isSignedIn.value ? connect() : disconnect();
+});
 
 const actions = {
-  ask_identity(ws, channel, type, data) {
-      const { id: socketId } = data;
-      
-      const user = {
-          id: 0,
-          firstname: 'Nicolas',
-          lastname: 'Choquet',
-          email: 'nchoquet@norsys.fr',
-          repos_pseudo: {
-              github: 'nicolachoquet06250',
-              gitlab: 'nicolachoquet06250'
-          },
-          followed_apps: [1]
-      };
+    ask_identity(ws, channel, _type, data) {
+        const { guest } = useGuest();
 
-      ws.send(JSON.stringify({
-          channel,
-          type: 'give',
-          data: { user, id: socketId }
-      }))
+        const { id: socketId } = data;
+        
+        const user = Object.keys(guest.value).reduce((r, c) => c === 'password' ? r : { ...r, [c]: guest.value[c] }, {});
+
+        ws.send(JSON.stringify({
+            channel,
+            type: 'give',
+            data: { user, id: socketId }
+        }))
     },
 
     received_identity() {
         console.log('identitée bien reçue');
+        openToast('Votre identité à bien été envoyé au serveur de notifications', 4000);
     },
 
-    give_notify(channel, type, data) {
-      console.log(data);
+    give_notify(_ws, _channel, _type, data) {
+        const { guest } = useGuest();
+
+        const { type: subType, appId, appName, users } = data;
+
+        const concerned = users.indexOf(guest.value.id) !== -1;
+
+        if (concerned) {
+            const { app } = useApp(parseInt(appId));
+            const name = app.value ? app.value.name : appName;
+            openToast(`L'application ${name} ${subType === 'updated' ? `vient d'être mise à jour` : 'vient de sortir'}`, 4000);
+        }
     }
 };
 
 addWsEventListener('message', (ws, event) => {
-    const { data } = event;
-    const { channel, type, data: _data } = JSON.parse(data);
+    const { channel, type, data } = JSON.parse(event.data);
     
     if (`${type}_${channel}` in actions) {
-        actions[`${type}_${channel}`](ws, channel, type, _data);
+        actions[`${type}_${channel}`](ws, channel, type, data);
     } else {
-        console.log(JSON.parse(data))
+        console.log(JSON.parse(event.data))
     }
 });
+
+addEventListener('close', () => {
+  console.log('le socket à été fermé proprement');
+})
 
 resize(() => {
   marginTop.value = offsetHeight.value;
--- src/hooks/websocket.js
+++ src/hooks/websocket.js
@@ -1,6 +1,7 @@
 import { ref, reactive } from 'vue';
 
 const ws = ref(null);
+const connected = ref(false);
 const callbacks = reactive({});
 
 export const useWebsocket = url => ({
@@ -8,6 +9,13 @@ export const useWebsocket = url => ({
         if (!ws.value) {
             const protocol = window.location.protocol.indexOf('https') !== -1 ? 'wss' : 'ws';
             ws.value = new WebSocket(`${protocol}://${url}`);
+            connected.value = true;
+
+            if (Object.keys(callbacks).length > 0) {
+                for (const key of Object.keys(callbacks)) {
+                    ws.value?.addEventListener(key, (...args) => callbacks[key](ws.value, ...args));
+                }
+            }
         }
 
         return ws.value;
@@ -15,16 +23,24 @@ export const useWebsocket = url => ({
 
     disconnect() {
         if (ws.value) {
+            if (Object.keys(callbacks).length > 0) {
+                for (const key of Object.keys(callbacks)) {
+                    ws.value?.removeEventListener(key, (...args) => callbacks[key](ws.value, ...args));
+                }
+            }
+
             ws.value.close();
+            ws.value = null;
+            connected.value = false;
         }
     },
 
     addWsEventListener(event, callback) {
         if (callbacks[event]) {
-            ws.value.removeEventListener(event, (...args) => callbacks[event](ws.value, ...args));
+            ws.value?.removeEventListener(event, (...args) => callbacks[event](ws.value, ...args));
         }
 
         callbacks[event] = callback;
-        ws.value.addEventListener(event, (...args) => callbacks[event](ws.value, ...args));
+        ws.value?.addEventListener(event, (...args) => callbacks[event](ws.value, ...args));
     }
 });
